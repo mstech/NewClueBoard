f543950c633340726a40fa8c5f26cbc15f0deb51
Aakash Shah <ashah@mines.edu>
2012-10-26 16:51:40 -0600

    Added a stub test methods

---
+++ Test/GameSetupTests.java
@@ -0,0 +1,55 @@
+package Test;
+
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.BoardCell;
+
+
+public class GameSetupTests {
+	private static Board board;
+	
+	@BeforeClass
+	public static void setUp() {
+		board = new Board();
+	}
+	
+	@Test
+	public void testLoadingPeople() {
+		
+	}
+	
+	@Test
+	public void testLoadingcards() {
+		
+	}
+	
+	@Test
+	public void testUniqueAndFairCardDistribution() {
+		
+	}
+	
+	@Test
+	public void testAccusation() {
+		
+	}
+	
+	@Test
+	public void testDisprovingSuggestion() {
+		
+	}
+	
+	@Test
+	public void testSelectingTarget() {
+		
+	}
+	
+	@Test
+	public void testMakingSuggestion() {
+		
+	}
+	
+}

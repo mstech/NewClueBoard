35e532956bf4bf3e9d87d505e7319d59665a426d
Matthew Stech <mstech@mymail.mines.edu>
2012-10-29 16:18:58 -0600

    Fixed bug in Human player

--- clueGame/HumanPlayer.java
+++ clueGame/HumanPlayer.java
@@ -3,7 +3,7 @@ package clueGame;
 public class HumanPlayer extends Player {
 
 	public HumanPlayer(String name, int startX, int startY) {
-		super(name, startX, startX);
+		super(name, startX, startY);
 	}
 
 }
